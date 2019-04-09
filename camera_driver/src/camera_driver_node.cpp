b621d42114b16c0467cfc42f107ab80cda3f7ab9
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2019-04-09 13:37:57 +0200

    Added camera info publisher

--- camera_driver/src/camera_driver_node.cpp
+++ camera_driver/src/camera_driver_node.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <vector>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
@@ -9,17 +10,31 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "image_publisher");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("/ropod/camera/front", 1);
+    image_transport::Publisher camera_img_pub = it.advertise("/ropod/camera/front", 1);
+
+    ros::Publisher camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("/ropod/camera/front/info", 1);
+    sensor_msgs::CameraInfo ropod_camera_front_info;
+    ropod_camera_front_info.D = {0,0,0,0,0};
 
     double frequency;
-    nh.param<double>("frequency", frequency, 20);
-    ROS_DEBUG("Frequency set to %0.1f Hz", frequency);
+    nh.param<double>("/camera_driver_node/frequency", frequency, 20);
+    ROS_INFO("Frequency set to %0.1f Hz", frequency);
 
     int device_id;
-    nh.param<int>("device_id", device_id, 0);
-    ROS_DEBUG("Device id set to %d ", device_id);
+    nh.param<int>("/camera_driver_node/device_id", device_id, 0);
+    ROS_INFO("Device id set to %d ", device_id);
 
+    std::vector<double> camera_matrix;
+    nh.getParam("/camera_driver_node/camera_matrix/data", camera_matrix);
+    std::copy(camera_matrix.begin(), camera_matrix.end(), ropod_camera_front_info.K.begin());
 
+    std::vector<double> distortion_coefficients;
+    nh.getParam("/camera_driver_node/distortion_coefficients/data", distortion_coefficients);
+    std::copy(distortion_coefficients.begin(), distortion_coefficients.end(), ropod_camera_front_info.D.begin());
+
+    double avg_reprojection_error;
+    nh.getParam("/camera_driver_node/avg_reprojection_error", avg_reprojection_error);
+    
     cv::VideoCapture cap(device_id);
     // Check if video device can be opened with the given index
     if (!cap.isOpened()) return 1;
@@ -34,10 +49,11 @@ int main(int argc, char** argv)
         if (!frame.empty()) 
         {
             msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-            pub.publish(msg);
+            camera_img_pub.publish(msg);
+
+            camera_info_pub.publish(ropod_camera_front_info);
             cv::waitKey(1);
         }
-
         ros::spinOnce();
         loop_rate.sleep();
     }
